ddced2df74c026751ae6d3a1344aed78b0cae005
Jancikos <ko.janci@gmail.com>
2025-12-17 00:08:09 +0100

    triedy

---
+++ APIS.SPSJM.OOP.Triedy/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using APIS.SPSJM.OOP.Triedy;
+
+var sklenik1 = new Sklenik()
+{
+    Senzory =
+    {
+        new Senzor() { Teplota = 22.5, Vlhkost = 55.0 },
+        new Senzor() { Teplota = 23.0, Vlhkost = 50.0 }
+    },
+    OsvetlenieZapnute = true
+};
+
+var sklenik2 = new Sklenik()
+{
+    Senzory =
+    {
+        new Senzor() { Teplota = 20.0, Vlhkost = 60.0 },
+        new Senzor() { Teplota = 21.5, Vlhkost = 58.0 }
+    },
+    OsvetlenieZapnute = false
+};
+
+Console.WriteLine($"#1 {sklenik1.}");
+Console.WriteLine($"#2 {sklenik2}");
+
---
+++ APIS.SPSJM.OOP.Triedy/Sklenik.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIS.SPSJM.OOP.Triedy;
+
+public class Sklenik
+{
+    public List<Senzor> Senzory { get; set; }
+
+
+    public bool OsvetlenieZapnute { get; set; }
+
+    private bool _dvereOtvorene = false;
+    public bool DvereOtvorene
+    {
+        get { return _dvereOtvorene; }
+        set
+        {
+            _dvereOtvorene = value;
+        }
+    }
+
+    // Konštruktor
+    public Sklenik()
+    {
+        Senzory = new List<Senzor>();
+        OsvetlenieZapnute = false;
+    }
+
+    // Verejná metóda
+    public double PriemernaTeplota()
+    {
+        if (Senzory.Count == 0)
+            return 0.0;
+
+        double sumaTeplot = 0.0;
+        foreach (var senzor in Senzory)
+        {
+            sumaTeplot += senzor.Teplota;
+        }
+        return sumaTeplot / Senzory.Count;
+    }
+
+    // Verejná metóda (použitie LINQ a arrow function syntax)
+    public double PriemernaVlhkost() => (Senzory.Count == 0) ? 0.0 : Senzory.Average(s => s.Vlhkost);
+
+    // Verejná metóda, preťažená (override) z predka Object
+    public override string ToString()
+    {
+        return $"Sklenik: Priemerna Teplota = {PriemernaTeplota():F2} °C, Priemerna Vlhkost = {PriemernaVlhkost():F2} %, Osvetlenie = {(OsvetlenieZapnute ? "Zapnute" : "Vypnute")}";
+    }
+}
