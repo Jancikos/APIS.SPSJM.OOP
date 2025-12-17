81dd021e6b3518d0acdc0442b34bd2f0eb8a2059
Jancikos <ko.janci@gmail.com>
2025-12-17 01:23:34 +0100

    dedenie

---
+++ APIS.SPSJM.OOP.Dedenie/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using APIS.SPSJM.OOP.Dedenie;
+
+var sklenik1 = new Sklenik()
+{
+    Senzory =
+    {
+        // new Senzor(), // Toto by spôsobilo chybu, pretože Senzor je abstraktná trieda
+        new BoschSenzor(),
+        new BoschSenzor()
+    },
+    OsvetlenieZapnute = true
+};
+
+var sklenik2 = new Sklenik()
+{
+    Senzory =
+    {
+        new XiaomiSenzor(),
+        new XiaomiSenzor(),
+        new BoschSenzor(),
+        new BoschSenzor(),
+        new XiaomiSenzor()
+    },
+    OsvetlenieZapnute = false
+};
+
+Console.WriteLine($"#1 {sklenik1}");
+Console.WriteLine($"#2 {sklenik2}");
---
+++ APIS.SPSJM.OOP.Dedenie/Senzor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIS.SPSJM.OOP.Dedenie
+{
+    // Abstraktna trieda - nemôže byť inštanciovaná priamo
+    public abstract class Senzor
+    {
+        // Statický atribút - zdieľaný medzi všetkými inštanciami triedy
+        public static int TotalSensorsCreated { get; private set; } = 0;
+        public int ID { get; init; }
+
+        protected double _posledneNameranaTeplota;
+        public virtual double Teplota => _posledneNameranaTeplota;
+        protected double _posledneNameranaVlhkost;
+        public virtual double Vlhkost => _posledneNameranaVlhkost;
+
+        // Konštruktor
+        public Senzor()
+        {
+            ID = ++TotalSensorsCreated;
+            SpustiMeranie();
+        }
+
+        // Abstraktná metóda - musí byť implementovaná v odvodených triedach
+        public abstract void SpustiMeranie();
+    }
+
+    public class BoschSenzor : Senzor
+    {
+        // implementácia abstraktnej metódy
+        public override void SpustiMeranie()
+        {
+            var rand = new Random();
+            _posledneNameranaTeplota = rand.NextDouble() * 40.0;
+            _posledneNameranaVlhkost = rand.NextDouble() * 100.0;
+        }
+    }
+
+    public class XiaomiSenzor : Senzor
+    {
+        // zavedenie atributu koeficientu vlhkosti, iba pre tento typ senzora
+        private double _koeficientVlhkosti;
+
+        // prepis getera vlastnosti Vlhkost
+        public override double Vlhkost => _posledneNameranaVlhkost * _koeficientVlhkosti;
+
+        // implementácia abstraktnej metódy
+        public override void SpustiMeranie()
+        {
+            _posledneNameranaTeplota = Random.Shared.NextDouble() * 40.0;
+            _posledneNameranaVlhkost = Random.Shared.NextDouble() * 100.0;
+            _koeficientVlhkosti = 0.8 + Random.Shared.NextDouble();
+        }
+    }
+}
